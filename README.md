c0f123689866fba727d47d4a9324b2c3fb4be776
Peter Scriven <pjpscriv@gmail.com>
2025-04-09 16:24:00 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# StackOverflow Prettifier Chrome Extension
+
+This Chrome extension enhances GitHub profiles and Gist pages by prettifying unsupported social links. It replaces standard links with visually appealing icons, making it easier for users to identify social profiles.
+
+## Developement
+
+Launch the extension using Mozilla's `web-ext`
+
+```sh
+web-ext run --target chromium --source-dir ./dist/
+```
+
+### Package
+
+```sh
+web-ext build --source-dir ./dist/
+```
+
+## Contributing
+
+Contributions are welcome! If you have suggestions or improvements, feel free to open an issue or submit a pull request.
---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+const path = require('path');
+const copyWebpackPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  devtool: 'inline-source-map',
+  entry: './src/index.ts',
+  output: {
+    filename: 'content.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  plugins: [
+    new copyWebpackPlugin({
+      patterns: [
+        {
+          from: path.resolve(__dirname, 'src/icons'),
+          to: path.resolve(__dirname, 'dist/icons')
+        },
+        {
+          from: path.resolve(__dirname, 'manifest.json'),
+          to: path.resolve(__dirname, 'dist/manifest.json'),
+        }
+      ]
+    })
+  ],
+  module: {
+    rules: [
+      { 
+        test: /\.tsx?$/,
+        loader: "ts-loader"
+      }
+    ]
+  },
+	resolve: {
+		extensions: ['.tsx', '.ts', '.js'],
+	},
+};
